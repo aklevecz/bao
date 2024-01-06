9c89193cdb3455717e4da0d7144c9eb2d5e69975
aklevecz <teh@raptor.pizza>
2024-01-05 20:33:25 -0800

    border

--- src/routes/baostagram/+page.svelte
+++ src/routes/baostagram/+page.svelte
@@ -10,11 +10,20 @@
   /** @type {string[]} */
   let blobUrls = [];
 
-  const resizePrefix = `https://baos.haus/cdn-cgi/image/width=${window.innerWidth - 50},height=auto,quality=100,fit=contain`
-  const r2Preix = 'https://r2.baos.haus'
   onMount(async () => {
+    /** @type {number | string}*/
+    let width = window.innerWidth;
+    /** @type {number | string}*/
+    let height = "auto"
+    if (width > 768) {
+      height = window.innerHeight - 200;
+      width = "auto";
+    }
+    console.log(height, width)
+    const resizePrefix = `https://baos.haus/cdn-cgi/image/width=${width},height=${height},quality=100,fit=contain`;
+    const r2Preix = "https://r2.baos.haus";
     const imgObjects = await fetchAllBaostagramUrls();
-    console.log(imgObjects)
+    console.log(imgObjects);
     // @ts-ignore
     const keys = imgObjects.sort((a, b) => new Date(b.lastModified).getTime() - new Date(a.lastModified).getTime()).map((o) => o.key);
     // @ts-ignore
@@ -31,17 +40,20 @@
 </script>
 
 <div class="flex flex-col items-center gap-4">
-    <h1>BAOSTAGRAM</h1>
-    {#each imgs as url}
-      <img loading="lazy" class="max-w-3xl my-8 w-11/12" src={url} alt="chicken" />
-    {/each}
+  <h1>BAOSTAGRAM</h1>
+  {#each imgs as url}
+    <img loading="lazy" class="max-w-3xl my-8 w-11/12" src={url} alt="chicken" />
+  {/each}
 </div>
 
 <style>
-    h1 {
-        @apply text-5xl font-bold text-black bg-white py-1 px-4 rounded-md;
-    }
-    img {
-      min-height: 400px;
-    }
+  h1 {
+    @apply text-5xl font-bold text-black bg-white py-1 px-4 rounded-md;
+  }
+  img {
+    min-height: 400px;
+    max-width: 60vh;
+    padding: 40px;
+    background: radial-gradient(#2a00ff, #d2d3ff63);
+  }
 </style>
